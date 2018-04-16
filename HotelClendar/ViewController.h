2c4e6128f1dbe227bbebed84fbc5866e941ff937
zhaoHm <zhao@allinpalm.com>
2018-04-16 22:59:10 +0800

    Initial Commit

---
+++ HotelClendar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HotelClendar
+//
+//  Created by 赵海明 on 2018/4/16.
+//  Copyright © 2018年 cnmobi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HotelClendar/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HotelClendar
+//
+//  Created by 赵海明 on 2018/4/16.
+//  Copyright © 2018年 cnmobi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
